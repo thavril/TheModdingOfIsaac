8624e3e3e1fb62410554a4c65b2044b4cbaea56b
thomasAvril <thomas.avril83@gmail.com>
2017-01-18 14:04:03 +0100

    update to start trying to remove damages from spikes

--- brave_shoe/main.lua
+++ brave_shoe/main.lua
@@ -1,14 +1,34 @@
+StartDebug()
 local debug = require('debug');
 local xahos = RegisterMod("The one Shoe to rule them all", 1);
 local brave_shoe_item = Isaac.GetItemIdByName("Brave Shoe");
 
-debug_text = "u has no item"
+debug_text = "pas ditems frerot"
+
+function xahos:item_effect(current_player)
+  player = Isaac.GetPlayer(0)
+  
+  if player:HasCollectible(brave_shoe_item) then
+    room = Game():GetRoom();
+    grid_size = room:GetGridSize();
+    
+    debug_text = "jai la size"
+    for i = 0, grid_size-1 do
+      grid_entity = room:GetGridEntity(i);
+      if (grid_entity ~= nil) then
+        is_spike = grid_entity:ToSpikes();
+        if (is_spike) then
+          room:DestroyGrid(i, true)
+        end
+      end
+    end
+  end
+end
 
 function xahos:cacheUpdate(player, cacheFlag)
   if player:HasCollectible(brave_shoe_item) then
     if (cacheFlag == CacheFlag.CACHE_SPEED) then
-      debug_text = "speed up";
-      player.MoveSpeed = player.MoveSpeed + 2;
+      player.MoveSpeed = player.MoveSpeed + 0.2;
     end
   end
 end
@@ -17,5 +37,6 @@ function xahos:debug_text()
   Isaac.RenderText(debug_text, 100, 100, 255, 0, 0, 255)
 end
 
+xahos:AddCallback(ModCallbacks.MC_POST_UPDATE, xahos.item_effect, EntityType.ENTITY_PLAYER)
 xahos:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, xahos.cacheUpdate)
 xahos:AddCallback(ModCallbacks.MC_POST_RENDER, xahos.debug_text);
