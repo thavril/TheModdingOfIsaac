911836e0f3d6c5949ac3e027e421183411f2b167
thomas <thomas.avril83@gmail.com>
2017-01-18 22:28:28 +0100

    working brave_shoe mod !

--- brave_shoe/main.lua
+++ brave_shoe/main.lua
@@ -1,31 +1,23 @@
-StartDebug()
-local debug = require('debug');
-local xahos = RegisterMod("The one Shoe to rule them all", 1);
+--[[
+Item: "Brave Shoe"
+-xahos-
+--]]
+
+local Afterbrasse = RegisterMod("Brave Shoe", 1);
 local brave_shoe_item = Isaac.GetItemIdByName("Brave Shoe");
 
-debug_text = "pas ditems frerot"
-
-function xahos:item_effect(current_player)
+function Afterbrasse:take_damage(entity, dmg_amount, dmg_flag, dmg_src, dmg_countdown)
   player = Isaac.GetPlayer(0)
   
   if player:HasCollectible(brave_shoe_item) then
-    room = Game():GetRoom();
-    grid_size = room:GetGridSize();
-    
-    debug_text = "jai la size"
-    for i = 0, grid_size-1 do
-      grid_entity = room:GetGridEntity(i);
-      if (grid_entity ~= nil) then
-        is_spike = grid_entity:ToSpikes();
-        if (is_spike) then
-          room:DestroyGrid(i, true)
-        end
-      end
+    if (dmg_flag == DamageFlag.DAMAGE_SPIKES) then
+      return false
     end
   end
+  return true
 end
 
-function xahos:cacheUpdate(player, cacheFlag)
+function Afterbrasse:cacheUpdate(player, cacheFlag)
   if player:HasCollectible(brave_shoe_item) then
     if (cacheFlag == CacheFlag.CACHE_SPEED) then
       player.MoveSpeed = player.MoveSpeed + 0.2;
@@ -33,10 +25,5 @@ function xahos:cacheUpdate(player, cacheFlag)
   end
 end
 
-function xahos:debug_text()
-  Isaac.RenderText(debug_text, 100, 100, 255, 0, 0, 255)
-end
-
-xahos:AddCallback(ModCallbacks.MC_POST_UPDATE, xahos.item_effect, EntityType.ENTITY_PLAYER)
-xahos:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, xahos.cacheUpdate)
-xahos:AddCallback(ModCallbacks.MC_POST_RENDER, xahos.debug_text);
+Afterbrasse:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, Afterbrasse.take_damage, EntityType.ENTITY_PLAYER)
+Afterbrasse:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Afterbrasse.cacheUpdate)
